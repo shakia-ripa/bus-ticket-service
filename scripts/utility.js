f4281206e3323eb6127b63035c0ddb55e3dc8106
shakiaripa <shakiaripa@gmail.com>
2024-02-21 17:48:21 +0600

    added footer

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,4 +1,9 @@
 function scrollFunction() {
-    const element = document.getElementById("content");
-    element.scrollIntoView();
-  }
+  const element = document.getElementById("tickets-section");
+  element.scrollIntoView();
+}
+
+function setBGById(elementID){
+  const element = document.getElementById(elementID);
+  element.classList.add('bg-[#1DD100]')
+}
